81b59dc5995835d30bfb81ee1c63cdc0ef0e6f37
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-01 03:29:10 -0600

    Render IP location in Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,11 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 import "./Map.css";
 import iconLocation from "../images/icon-location.svg";
 
-const INITIAL_COORDS = [51.505, -0.09];
 const OFFSET = 0.0002;
 const ZOOM = 20;
 
@@ -18,28 +17,38 @@ const markerIcon = new L.Icon({
   shadowUrl: null,
   shadowSize: null,
   shadowAnchor: null,
-  iconSize: new L.Point(60, 75),
+  iconSize: new L.Point(46, 56),
 });
 
 const applyOffset = (coords) => {
   return [coords[0] + OFFSET, coords[1]];
 };
 
-export default function Map() {
+export default function Map({ coords }) {
   const [map, setMap] = useState(null);
-  const [markerPosition, setMarkerPosition] = useState(INITIAL_COORDS);
+  const [marker, setMarker] = useState(null);
 
+  const updateMarker = (coords) => {
+    if (marker !== null) {
+      map.removeLayer(marker);
+    }
+    let newMarker = L.marker(coords, { icon: markerIcon }).addTo(map);
+    setMarker(newMarker);
+  };
+
+  // Update Map view
   useEffect(() => {
-    if (map === null) return;
+    if (map === null || coords === null) return;
 
-    L.marker(markerPosition, { icon: markerIcon }).addTo(map);
-  }, [map, markerPosition]);
+    map.setView(applyOffset(coords), ZOOM);
+    updateMarker(coords);
+  }, [map, coords]);
 
   const displayMap = useMemo(
     () => (
       <MapContainer
         className="Map"
-        center={applyOffset(INITIAL_COORDS)}
+        center={applyOffset(coords)}
         zoom={ZOOM}
         zoomControl={false}
         whenCreated={setMap}
@@ -50,7 +59,7 @@ export default function Map() {
         />
       </MapContainer>
     ),
-    []
+    [coords]
   );
 
   return <div>{displayMap}</div>;
